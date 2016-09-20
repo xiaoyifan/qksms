4cbebdea628f1b4e10adb8cf17eb59ca14015dae
Yifan Xiao <yifan.xiao@sqor.com>
2016-09-20 12:48:48 -0700

    Implemented source and source loader for mentions.

--- QKSMS/src/main/java/com/moez/QKSMS/data/mentions/ServiceSource.java
+++ QKSMS/src/main/java/com/moez/QKSMS/data/mentions/ServiceSource.java
@@ -1,10 +1,15 @@
 package com.moez.QKSMS.data.mentions;
 
+import android.content.res.Resources;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.linkedin.android.spyglass.mentions.Mentionable;
+import com.moez.QKSMS.R;
+
+import org.json.JSONArray;
 
 /**
  * Created by yxiao on 9/20/16.
@@ -24,22 +29,29 @@ public class ServiceSource implements Mentionable {
     @NonNull
     @Override
     public String getTextForDisplayMode(MentionDisplayMode mode) {
-        return null;
+        switch (mode) {
+            case FULL:
+                return mName;
+            case PARTIAL:
+            case NONE:
+            default:
+                return "";
+        }
     }
 
     @Override
     public MentionDeleteStyle getDeleteStyle() {
-        return null;
+        return MentionDeleteStyle.PARTIAL_NAME_DELETE;
     }
 
     @Override
     public int getSuggestibleId() {
-        return 0;
+        return mName.hashCode();
     }
 
     @Override
     public String getSuggestiblePrimaryText() {
-        return null;
+        return mName;
     }
 
     @Override
@@ -48,8 +60,8 @@ public class ServiceSource implements Mentionable {
     }
 
     @Override
-    public void writeToParcel(Parcel parcel, int i) {
-
+    public void writeToParcel(Parcel dest, int i) {
+        dest.writeString(mName);
     }
 
     public ServiceSource(Parcel in) {
@@ -66,4 +78,29 @@ public class ServiceSource implements Mentionable {
             return new ServiceSource[size];
         }
     };
+
+    public static class ServiceSourceLoader extends MentionsLoader<ServiceSource>{
+
+        private static final String TAG = ServiceSourceLoader.class.getSimpleName();
+
+        public ServiceSourceLoader(Resources res) {
+            super(res, R.raw.mention_source);
+        }
+
+        @Override
+        public ServiceSource[] loadData(JSONArray arr) {
+            ServiceSource[] data = new ServiceSource[arr.length()];
+            try {
+                for (int i = 0; i < arr.length(); i++) {
+                    data[i] = new ServiceSource(arr.getString(i));
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Unhandled exception while parsing city JSONArray", e);
+            }
+            return data;
+        }
+
+    }
+
+
 }
